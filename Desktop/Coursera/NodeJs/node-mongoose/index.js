ea2b23a45b7a3f1870aea389a45ece083e580d63
harishikhar1997 <harishikhar1997@gmail.com>
2018-11-30 13:57:15 +0530

    Node Mongoose Exercise 3

--- Desktop/Coursera/NodeJs/node-mongoose/index.js
+++ Desktop/Coursera/NodeJs/node-mongoose/index.js
@@ -10,23 +10,39 @@ connect.then((db) =>{
     console.log('Connected correctly to server');
 
     Dishes.create({
-        name:'Uthappizza',
-        description:'test'
+        name: 'Uthappizza',
+        description: 'test'
     })
-    .then((dish)=>{
+    .then((dish) => {
         console.log(dish);
 
-        Dishes.find({}).exec();
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
     })
-    .then((dishes)=>{
-        console.log(dishes);
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
 
         return Dishes.remove({});
     })
-    .then(()=>{
+    .then(() => {
         return mongoose.connection.close();
     })
-    .catch((err)=>{
+    .catch((err) => {
         console.log(err);
     });
 });
